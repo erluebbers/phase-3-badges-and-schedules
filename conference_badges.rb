94427daf2eb85e9308c7149e9fe9fe9de6d24514
Erik Luebbers <erluebbers@gmail.com>
2022-02-07 16:43:40 -0800

    commit tree 57b3e32f3eadef4b7dbd033da5d694109e50e10c

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_arr)
+  name_arr.map { |name| "Hello, my name is #{name}."}
+end 
+
+def assign_rooms(name_arr)
+  new_arr = name_arr.map.with_index(1) { |name, idx| "Hello, #{name}! You'll be assigned to room #{idx}!" }
+  new_arr
+end
+
+def printer(arr)
+  batch_badge_creator(arr).each { |badge| puts badge }
+  assign_rooms(arr).each { |assignment| puts assignment}
+end
+
